e3623054698f06bc42aabb4f3e7b3180b47bca3d
Blazzzeee <legendgamer1007@gmail.com>
2026-01-09 23:40:26 +0530

    minor formatting fix

--- pkgs.nix
+++ pkgs.nix
@@ -2,22 +2,23 @@
 
 {
     environment.systemPackages = with pkgs; [
-      kitty
-      nerd-fonts.jetbrains-mono
-      vimix-cursors
-      wl-clipboard
-      pavucontrol
-      brightnessctl
-      arp-scan
-      tailscale
-      libnotify
-      dnsutils
-      clang-tools
-      lua-language-server
-      pyright
-      nodePackages.typescript-language-server
-      nodePackages.eslint
-      rubyPackages.ruby-lsp
-      emmet-language-server
+        kitty
+        nerd-fonts.jetbrains-mono
+        vimix-cursors
+        wl-clipboard
+        pavucontrol
+        brightnessctl
+        arp-scan
+        tailscale
+        libnotify
+        dnsutils
+        clang-tools
+        lua-language-server
+        pyright
+        nodePackages.typescript-language-server
+        nodePackages.eslint
+        rubyPackages.ruby-lsp
+        emmet-language-server
+        uv
     ];
 }

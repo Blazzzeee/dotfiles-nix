351bb53c7038eec43b11d4fb0bb74c4a2dbae298
Blazzzeee <legendgamer1007@gmail.com>
2026-01-20 22:39:52 +0530

    offload system bootloader from nixos (chainloaded_)

--- configuration.nix
+++ configuration.nix
@@ -11,8 +11,10 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+
+  boot.loader.efi.canTouchEfiVariables = false;
 
 
   # Use latest kernel.
